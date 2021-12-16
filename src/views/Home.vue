ccdafcc8c28f1ea34ac232af38c111ba3b1ed132
Axel CHEMIN <axel.chemin@polytech-lille.net>
2021-12-16 10:53:19 +0100

    use store to display card only when connected

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,32 @@
   <div class="home">
     <h1>Bienvenue sur le site Polylove</h1>
     <p>Ce site vous permettra de trouver l'amour dans votre réseau d'école ! Ou d'élargir vos horizons à de nouvelles filières</p>
-    <h2>Mon profil :</h2>
-    <v-container class="card-container">
-      <PublicProfile id-user:1/>
-    </v-container>
+    <div v-if="this.isLoggedIn">
+      <p>Vous devez d'abord vous connecter en entrant votre identifiant à haut à droite de votre écran</p>
+    </div>
+    <div v-else>
+      <h2>Mon profil :</h2>
+      <v-container class="card-container">
+        <PublicProfile id-user:1/>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import PublicProfile from "@/components/PublicProfile";
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
     PublicProfile
+  },
+  computed: {
+    ...mapState({
+      isLoggedIn: 'isConnected'
+    })
   }
 }
 </script>
